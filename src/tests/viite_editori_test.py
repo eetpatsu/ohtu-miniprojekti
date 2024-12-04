78e555f859645f173d5fb6b79d626135640c0d7e
lalaajal <lauri.j.laajala@student.jyu.fi>
2024-12-04 11:06:50 +0200

    Lisätty ensimmäinen testi viite-editorille

--- src/tests/viite_editori_test.py
+++ src/tests/viite_editori_test.py
@@ -1,31 +1,20 @@
-"""import unittest
+import unittest
 from viite_editori import ViiteEditori
-
 
 class TestViiteEditori(unittest.TestCase):
 
-
     def setUp(self):
-        #self.testiparseri = ViiteParseri(self.esimerkkiviite)
-        #self.testieditori = ViiteEditori(...)
-        
+        self.testieditori = ViiteEditori(DummyConsoleIO)
 
-# Kopioitu toisesta testitiedostosta
+    def test_tulosta_tiedosto(self):
+        nimi = "testitiedosto.bib"
+        self.testieditori.avaa_tiedosto(nimi)
+        self.assertEqual(self.testieditori.Tulosta_tiedosto(nimi), 0)
 
-    def test_konstruktori_ja_to_string(self):
-        self.assertAlmostEqual(str(self.testiparseri), self.esimerkkiviite)
 
-    def test_viitteen_tyyppi(self):
-        self.assertEqual(self.testiparseri.viitteen_tyyppi, "article")
+class DummyConsoleIO:
+    def kirjoita(str):
+        return
+    def lue():
+        return
 
-    def test_viitteen_avain(self):
-        self.assertEqual(self.testiparseri.viitteen_avain, "kadiyala2018applications")
-
-    def test_viitteen_tiedot(self):
-        self.assertEqual(self.testiparseri.viitteen_tiedot[0][0], "title")
-        self.assertEqual(self.testiparseri.viitteen_tiedot[0][1], "Applications of python to evaluate the performance of decision tree-based boosting algorithms")
-        #self.assertEqual(self.testiparseri.viitteen_tiedot[0], "title={Applications of python to evaluate the performance of decision tree-based boosting algorithms},")
-        #self.assertEqual(self.testiparseri.viitteen_tiedot[1], "author={Kadiyala, Akhil and Kumar, Ashok},")
-        #self.assertEqual(self.testiparseri.viitteen_tiedot[2], "journal={Environmental Progress \& Sustainable Energy},")
-
-"""
