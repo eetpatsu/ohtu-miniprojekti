5ad092d8ab81ddfdc612318a3a4e32292621667a
ollvilhi <ollvilhi@jyu.fi>
2024-12-05 10:20:44 +0200

    Tiedoston avaamisen yksikkötestausta

--- src/tests/viite_editori_test.py
+++ src/tests/viite_editori_test.py
@@ -1,12 +1,36 @@
 import unittest
 import os
 from viite_editori import ViiteEditori
+from pathlib import Path
 
 class TestViiteEditori(unittest.TestCase):
 
     def setUp(self):
         self.testieditori = ViiteEditori(DummyConsoleIO())
+        self.testitiedosto = "testi.bib"
 
+    def test_avaa_tiedosto_onnistuneesti(self):
+        # Luo testitiedoston
+        with open(self.testitiedosto, "w") as f:
+            f.write("Testitiedoston sisältö")
+        self.testieditori.avaa_tiedosto(self.testitiedosto)
+        self.assertEqual(str(self.testieditori.aktiivinen_tiedosto), str(Path(self.testitiedosto).resolve()))
+
+    def test_tiedosto_lisataan_paatteella(self):
+        tiedostonimi_ilman_paatetta = "tiedosto_ilman_paatetta"
+        # Luo testitiedoston .bib-päätteellä
+        with open(tiedostonimi_ilman_paatetta + ".bib", "w") as f:
+            f.write("Sisältöä")
+        self.testieditori.avaa_tiedosto(tiedostonimi_ilman_paatetta)
+        odotettu_polku = Path(tiedostonimi_ilman_paatetta + ".bib").resolve()
+        self.assertEqual(self.testieditori.aktiivinen_tiedosto, odotettu_polku)
+
+    def test_avaa_tiedosto_ilman_paatetta_luodaan_polku(self):
+        tiedostonimi_ilman_paatetta = "tiedosto_ilman_paatetta"
+        self.testieditori.avaa_tiedosto(tiedostonimi_ilman_paatetta)
+        odotettu_polkumuoto = os.path.join(os.getcwd(), tiedostonimi_ilman_paatetta + ".bib")
+        self.assertEqual(str(self.testieditori.aktiivinen_tiedosto), odotettu_polkumuoto)
+        
     def test_tiedosto_ei_auki(self):
         self.testieditori.aktiivinen_tiedosto = None
         result = self.testieditori.syota_bib_viite()
