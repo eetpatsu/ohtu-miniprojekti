745817536b5118cfd7eb0751e8d2fff7a3007fcc
Elias Lehtinen <elias.a.lehtinen@gmail.com>
2024-12-16 17:46:35 +0200

    Dokumentaatiota KomentolukijaLibraryyn ja [Tulosteen Tulisi Sisaltaa] hyväksyy enemmän whitespacea välilyönnin tilalla

--- src/KomentolukijaLibrary.py
+++ src/KomentolukijaLibrary.py
@@ -1,3 +1,4 @@
+import re
 from viite_editori import ViiteEditori
 from komentolukija import Komentolukija
 from console_io import ConsoleIO
@@ -9,23 +10,35 @@ class KomentolukijaLibrary:
         self._komentolukija = Komentolukija(self._io, self._viite_editori)
 
     def kaynnista_sovellus(self):
+        '''Kutsuu samoja funktioita kuin sovellus käynnistettäessä. Ei kuitenkaan oikeasti käynnistä mitään.'''
         self._komentolukija.viite_editori.helppi()
 
     def kaynnista_sovellus_argumenteilla(self, argumentit):
+        '''Sama kuin edellinen, mutta tekee samat asiat kuin sovellus käynnistettäessä samoilla argumenteilla.'''
         self._komentolukija.viite_editori.parse_argumentti(["sovellus"] + argumentit.split())
         self._komentolukija.viite_editori.helppi()
 
     def syota_komento(self, komento):
+        '''Laittaa sovelluksen käsittelemään annetun komennon. 
+        Jos komennon seurauksena kysytään käyttäjältä lisää parametreja,
+        ne täytyy syöttää ennen funktion kutsumista. (Katso resource.robot "Syota Komennot")'''
         self._komentolukija.kasittele_syote(komento)
 
     def syota_data(self, data):
+        '''Syöttää käyttäjän antamaa dataa valmiiksi. Ohjelma lukee näitä sitä mukaa kun io:n funktiota "lue" kutsutaan.'''
         self._io.syota(data)
 
     def tulosteen_tulisi_sisaltaa(self, teksti):
+        '''Tarkistaa löytyykö tulosteesta (mistään) annettua merkkijonoa. Yhden välilyönnin tilalla sallitaan enemmänkin tilaa.'''
+        teksti_regex = teksti.replace(" ", "\\s+")
+        teksti_regex = teksti_regex.replace("{", "\\{")
+        teksti_regex = teksti_regex.replace("}", "\\}")
+        teksti_regex = teksti_regex.replace("[", "\\[")
+        teksti_regex = teksti_regex.replace("]", "\\]")
         tuloste = self._io.tuloste
         loytyi = False
         for t in tuloste:
-            if teksti in t:
+            if re.search(teksti_regex, t): # Etsitään tekstillä, mutta niin että välilyöntejä voi olla enemmän
                 loytyi = True
 
         if not loytyi:
@@ -39,10 +52,12 @@ class MockConsoleIO(ConsoleIO):
         self.tuloste = []
 
     def kirjoita(self, teksti):
+        '''Kirjoittaa tuloste-taulukkoon tekstin, jonka ohjelma tulostaisi konsoliin.'''
         self.tuloste.append(teksti)
         print(teksti)  # Debug-tuloste
 
     def lue(self, kehote):
+        '''Ottaa syotteet-taulukosta ensimmäisen alkion pois ja palauttaa sen kutsuvalle ohjelmalle.'''
         self.tuloste.append(kehote)
         if self.syotteet:
             print(f"Luetaan komento: {self.syotteet[0]}")  # Debug tuloste
@@ -50,5 +65,7 @@ class MockConsoleIO(ConsoleIO):
         return ""
 
     def syota(self, syote):
+        '''Kirjoittaa annetun syötteen taulukkoon syotteet. Muuta ei tehdä.
+        Ohjelma lukee seuraavan komennon syotteet-taulukosta aina kun funktiota "lue" kutsutaan.'''
         print(f"Syötetään: {syote}") # Debug
         self.syotteet.append(syote)
